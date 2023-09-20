14cf5d9b5470e7eef3a11096cf2b5458664b5401
KarolKarnas <karol.karnas@gmail.com>
2023-09-20 12:45:10 +0200

    Old files cleanup

--- frontend/src/services/productService.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { Product } from '../types/Product';
-
-const getAll = async () => {
-	const { data } = await axios.get<Product[]>('/api/products');
-	return data;
-};
-
-export default { getAll };
--- frontend/src/services/projectService.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { Project } from '../_types';
-
-const getAll = async () => {
-	const { data } = await axios.get<Project[]>('/api/projects');
-	return data;
-};
-
-export default { getAll };
