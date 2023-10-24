69d577f2ecca65817c49caf17d0a64543a1b8239
KarolKarnas <karol.karnas@gmail.com>
2023-10-24 13:32:27 +0200

    Add Before each test functionality in product_api
    - Clear all data, and Save fresh data to database before each test
    - Modify tests to use products data (not hardcoded, for example product.length, not 9)
    - Create test checking login with correct data

--- backend/data/users.ts
+++ backend/data/users.ts
@@ -1,23 +1,25 @@
 import bcrypt from 'bcryptjs';
 import { User } from '../types/User';
 
+export const password = '123456';
+
 const users: User[] = [
 	{
 		name: 'Admin User',
 		email: 'admin@email.com',
-		password: bcrypt.hashSync('123456', 10),
+		password: bcrypt.hashSync(password, 10),
 		isAdmin: true,
 	},
 	{
 		name: 'John Doe',
 		email: 'john@email.com',
-		password: bcrypt.hashSync('123456', 10),
+		password: bcrypt.hashSync(password, 10),
 		isAdmin: false,
 	},
 	{
 		name: 'Jane Doe',
 		email: 'jane@email.com',
-		password: bcrypt.hashSync('123456', 10),
+		password: bcrypt.hashSync(password, 10),
 		isAdmin: false,
 	},
 ];
--- backend/tests/product_api.test.ts
+++ backend/tests/product_api.test.ts
@@ -1,9 +1,36 @@
 import mongoose from 'mongoose';
 import supertest from 'supertest';
 import app from '../app';
-// import { Product } from '../types/Product';
+import Product from '../models/productModel';
+import Order from '../models/orderModel';
+import User from '../models/userModel';
+import { products } from '../data/products';
+import users from '../data/users';
+import { ProductUser } from '../types/Product';
+import { password } from '../data/users';
 
 const api = supertest(app);
+
+beforeEach(async () => {
+	try {
+		await Order.deleteMany();
+		await Product.deleteMany();
+		await User.deleteMany();
+
+		const createdUsers = await User.insertMany(users);
+
+		const adminUser = createdUsers[0]._id;
+
+		const sampleProducts = products.map((product) => {
+			return { ...product, user: adminUser };
+		});
+
+		await Product.insertMany(sampleProducts);
+		console.log('Data Imported!');
+	} catch (error) {
+		console.error(`${error}`);
+	}
+});
 
 test('products are returned as json', async () => {
 	await api
@@ -12,18 +39,38 @@ test('products are returned as json', async () => {
 		.expect('Content-Type', /application\/json/);
 }, 100000);
 
-test('there are two 9 products', async () => {
+test('All products are returned', async () => {
 	const response = await api.get('/api/products');
 
-	expect(response.body).toHaveLength(9);
+	expect(response.body).toHaveLength(products.length);
 });
 
 test('the first product name is Basilisk', async () => {
 	const response = await api.get('/api/products');
 
-	// const products = response.body as Product[];
-
 	expect(response.body[0].name).toBe('Basilisk');
+});
+
+test('A specific product name is within the returned products', async () => {
+	const response = await api.get('/api/products');
+	const typedResponse = response.body as ProductUser[];
+	const names = typedResponse.map((product) => product.name);
+
+	expect(names).toContain('Forest Bobo');
+});
+
+test.only('A user can login with correct email and password', async () => {
+	const response = await api
+		.post('/api/users/auth')
+		.send({ email: users[0].email, password: password })
+		.expect(200);
+
+	console.log(response.body);
+
+	expect(response.body).toHaveProperty('_id');
+	expect(response.body).toHaveProperty('name');
+	expect(response.body).toHaveProperty('email');
+	expect(response.body).toHaveProperty('isAdmin');
 });
 
 afterAll(async () => {
