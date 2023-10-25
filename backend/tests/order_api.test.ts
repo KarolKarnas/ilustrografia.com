2f6f4b1722aee23685acdee9a39bfbcb81b84dc3
KarolKarnas <karol.karnas@gmail.com>
2023-10-25 13:12:39 +0200

    Add grouped tests (when 1 order is made)
    - check is 1 order already made (fetch orders by Admin),
    - can the user fetch his orders,
    - can the user pay for order

--- backend/tests/order_api.test.ts
+++ backend/tests/order_api.test.ts
@@ -43,9 +43,6 @@ beforeEach(async () => {
 		jwtUserToken = jwtUserCookie.split('=')[1].split(';')[0];
 		//
 
-		console.log(jwtAdminToken);
-		console.log(jwtUserToken);
-
 		const sampleProducts = products.map((product) => {
 			return { ...product, user: adminUser };
 		});
@@ -86,7 +83,7 @@ test('user can make an order', async () => {
 		totalPrice: 11,
 	};
 
-await api
+	await api
 		.post('/api/orders')
 		.set('Cookie', `jwt=${jwtUserToken}`)
 		.send(order);
@@ -98,6 +95,79 @@ await api
 	expect(responseOrdersBefore.body.length + 1).toBe(
 		responseOrdersAfter.body.length
 	);
+});
+
+describe('when 1 order is made', () => {
+	beforeEach(async () => {
+		const responseProducts = await api.get('/api/products');
+
+		const variation = responseProducts.body[0].variations[0] as VariationCart;
+
+		variation.qty = 10;
+		variation.image = 'test.jpg';
+		variation.variationName = `variation-test-name`;
+		variation.pathnameWithQuery = 'someTest/path';
+
+		const order: OrderData = {
+			orderItems: [variation],
+			shippingAddress: {
+				address: 'Old-town 37',
+				city: 'New York',
+				postalCode: '222',
+				country: 'USA',
+			},
+			paymentMethod: 'Pay Pal',
+			itemsPrice: 11111,
+			taxPrice: 2222,
+			shippingPrice: 333,
+			totalPrice: 11,
+		};
+
+		await api
+			.post('/api/orders')
+			.set('Cookie', `jwt=${jwtUserToken}`)
+			.send(order);
+	});
+
+	test('is there 1 order made', async () => {
+		const responseOrders = await api
+			.get('/api/orders')
+			.set('Cookie', `jwt=${jwtAdminToken}`);
+
+		expect(responseOrders.body.length).toBe(1);
+	});
+
+	test('user can get his/her orders', async () => {
+		const responseOrderData = await api
+			.get('/api/orders/my')
+			.set('Cookie', `jwt=${jwtUserToken}`);
+
+		expect(responseOrderData.body.length).toBe(1);
+	});
+	test('user can get pay for order', async () => {
+		const responseOrderData = await api
+			.get('/api/orders/my')
+			.set('Cookie', `jwt=${jwtUserToken}`);
+
+		const paidStatusBefore = responseOrderData.body[0].isPaid as boolean;
+
+		const id = responseOrderData.body[0]._id as string;
+
+		const responsePay = await api
+			.put(`/api/orders/${id}/pay`)
+			.set('Cookie', `jwt=${jwtUserToken}`)
+			.send({
+				id: id,
+				status: 'some_status',
+				update_time: 'some_time',
+				payer: { email_address: users[1].email },
+			});
+
+		const paidStatusAfter = responsePay.body.isPaid as boolean;
+
+		expect(paidStatusBefore).toBeFalsy();
+		expect(paidStatusAfter).toBeTruthy();
+	});
 });
 
 afterAll(async () => {
